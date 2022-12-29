6efea10563cb9138aea1cd871c5ab52415473fa1
Evgeny Loshkarev <evlosh@gmail.com>
2022-12-29 17:07:28 +0300

    removed restricted words

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
+import string
+import sqlalchemy
+from database import Dbase, Users, Words
+
 def sort_words(input: tuple):
     unic_words = set(i[0] for i in input)
     result = []
@@ -9,5 +13,23 @@ def sort_words(input: tuple):
     return tuple(reversed(sorted(result, key=lambda x: x[1])))
 
 
-tst = (('это', 3), ('это', 1), ('какое-то слово', 1), ('это', 2), ('другое слово', 3))
-print(sort_words(tst))
+def chat_words(msg_chat_id):
+    q = sqlalchemy.select(Words.word, Words.count).where(
+        Words.chat_id==-1001297579871).order_by(-Words.count)
+    db_words = Dbase.conn.execute(q).fetchall()
+    sorted = sort_words(db_words)[:150]
+    return list(i[0] for i in sorted)
+
+restricted = (
+    'это', 'что', 'так', 'все', 'как', 'там', 'меня', 'уже', 'вот', 
+    'где', 'если', 'есть', 'раз', 'нет', 'мне', 'для', 
+    'кто', 'они', 'она', 'тоже', 'чем', 'тебя',
+    'его', 'зачем', 'топ', 'или', 
+    'ещё', 'тут', 'был', 'нас', 
+    'про', 'еще', 'вас', 'чего'
+    )
+
+
+for i in restricted:
+    q = sqlalchemy.delete(Words).where(Words.word==i)
+    Dbase.conn.execute(q)
