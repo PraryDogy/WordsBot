d881a08e82b345ef8e00eb6ae5c31fb99642153d
Evgeny Loshkarev <evlosh@gmail.com>
2022-12-29 17:43:04 +0300

    refac

--- utils.py
+++ utils.py
@@ -79,40 +79,24 @@ def write_db(msg_user_id, msg_chat_id, msg_words):
             Dbase.conn.execute(q)
 
 
-def users_list(data):
-    user_ids = []
-    users = []
-    for id, name, _ in tuple(reversed(data)):
-        if id not in user_ids:
-            user_ids.append(id)
-            users.append((id, name))
-    return users
-
-
-def new_user(msg_user_id: int, msg_user_name: str):
-    vals = {'user_id': msg_user_id, 'user_name': msg_user_name}
-    new_user = sqlalchemy.insert(Users).values(vals)
-    Dbase.conn.execute(new_user)
-
-
-def update_name(input_user_id, new_user_name):
-    print('update name')
-    vals = {'user_name': new_user_name}
-    new_name = sqlalchemy.update(Users).where(
-        Users.user_id==input_user_id).values(vals)
-    Dbase.conn.execute(new_name)
-
-
 def check_user(msg_user_id: int, msg_user_name: str):
     get_user = sqlalchemy.select(
         Users.user_id, Users.user_name).filter(Users.user_id == msg_user_id)
     db_user = Dbase.conn.execute(get_user).first()
     
     if db_user is not None:
+
         db_usr_id, db_usr_name = db_user
+
         if db_usr_name != msg_user_name:
-            update_name(db_usr_id, msg_user_name)
+
+            vals = {'user_name': msg_user_name}
+            new_name = sqlalchemy.update(Users).where(
+                Users.user_id==db_usr_id).values(vals)
+            Dbase.conn.execute(new_name)
     else:
-        new_user(msg_user_id, msg_user_name)
+        vals = {'user_id': msg_user_id, 'user_name': msg_user_name}
+        new_user = sqlalchemy.insert(Users).values(vals)
+        Dbase.conn.execute(new_user)
 
 
