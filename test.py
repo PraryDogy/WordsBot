2d15bdd9f9dd01f7cfd9b0c3dea990085e746dee
Evgeny Loshkarev <evlosh@gmail.com>
2023-01-09 17:27:41 +0300

    text analyzer

--- test.py
+++ test.py
@@ -1,6 +1,27 @@
-import random
+import spacy
+from nltk.tokenize import RegexpTokenizer
+import string
+import re
+from datetime import datetime
+
+top_words = "мочь просто хороший весь знать год вообще хотеть говорит большой"
+
+with open('test_text.txt', 'r') as file:
+    data = file.read()
+
+def words_convert(text: str):
+    punct = str.maketrans(string.punctuation, ' '*len(string.punctuation))
+    rem_punct = text.translate(str.maketrans(punct))
+    rem_spaces = " ".join(rem_punct.split())
+    lower = rem_spaces.lower()
+
+    return lower
 
 
-a = tuple(i for i in range(100))
+# def nouns(text):
+#     nlp = spacy.load("ru_core_news_md")
+#     doc = nlp(text)
 
+#     for token in doc:
+#         print(token.text, token.pos_)
 
---
+++ text_analyser.py
@@ -0,0 +1,35 @@
+import spacy
+import string
+from pymorphy2 import MorphAnalyzer
+from dicts import stop_words
+
+nlp = spacy.load("ru_core_news_md")
+lemmatizer = MorphAnalyzer()
+
+with open('test_text.txt', 'r') as file:
+    data = file.read()
+
+
+def words_convert(text: str):
+    """
+    Returns words list without punctuation, stopwords and in normal form.
+    """
+    punct = str.maketrans(string.punctuation, ' '*len(string.punctuation))
+    rem_punct = text.translate(str.maketrans(punct))
+    rem_spaces = rem_punct.split()
+    lower = tuple(i.lower() for i in rem_spaces)
+    lema = tuple(lemmatizer.parse(word)[0].normal_form for word in lower)
+    clean_words= tuple(i for i in lema if i not in stop_words)
+    return clean_words
+
+
+def nouns(text: str):
+    """
+    Returns nouns list from input text.
+    """
+    doc = nlp(text)
+    return tuple(t.text for t in doc if t.pos_ == 'NOUN')
+
+
+words_list = ' '.join(words_convert(data))
+nns = nouns(words_list)
