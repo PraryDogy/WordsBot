26bb25617e66e46a476f7054cc27c9513a69a84e
evlosh <evlosh@gmail.com>
2023-01-09 21:44:34 +0300

    bot config

---
+++ bot_config.py
@@ -0,0 +1,7 @@
+from aiogram import Bot, Dispatcher
+
+TOKEN = '5891470600:AAF1BYG6oTN1y8EM6A-hHFL8tfINii25zhM'
+TOKEN_TEST = '5823171932:AAFag_U_td1HCi_w4vmjzUFz6-5X8ITK0xY'
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
