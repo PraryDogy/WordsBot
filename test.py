29e53e2028b0850135bcf654e819d7d5a3811ed5
evlosh <evlosh@gmail.com>
2023-01-18 23:03:37 +0300

    добаботай

--- test.py
+++ test.py
@@ -24,35 +24,35 @@ def get_prep(words_list):
         if parsed == 'PREP':
             return word
 
-message = 'Во сколько лет я умру?'
-words_list = words_regex(message.lower())
+# message = 'Во сколько лет я умру?'
+# words_list = words_regex(message.lower())
 
-question_type = get_question_word(words_list)
-prep = get_prep(words_list)
+# question_type = get_question_word(words_list)
+# prep = get_prep(words_list)
 
 
-answ_src = random.choice(destiny_answer[question_type['key']])
-answ_res = []
-if prep:
-    prep = prep.replace('во', 'в')
-    answ_res.append(prep)
+# answ_src = random.choice(destiny_answer[question_type['key']])
+# answ_res = []
+# if prep:
+#     prep = prep.replace('во', 'в')
+#     answ_res.append(prep)
 
-for i in answ_src.split():
-    parsed = morph.parse(i)[0]
+# for i in answ_src.split():
+#     parsed = morph.parse(i)[0]
 
-    res = parsed.inflect({
-        question_type['number'], question_type['case']})
+#     res = parsed.inflect({
+#         question_type['number'], question_type['case']})
 
-    if question_type['gender']:
-        parsed = morph.parse(res.word)[0]
-        res = parsed.inflect({question_type['gender']})
+#     if question_type['gender']:
+#         parsed = morph.parse(res.word)[0]
+#         res = parsed.inflect({question_type['gender']})
 
-    if res:
-        answ_res.append(res.word)
-    else:
-        answ_res.append(i)
+#     if res:
+#         answ_res.append(res.word)
+#     else:
+#         answ_res.append(i)
 
-print()
-print(message)
-print(' '.join(answ_res).capitalize())
-print()
+# print()
+# print(message)
+# print(' '.join(answ_res).capitalize())
+# print()
