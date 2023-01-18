3b649413380f74ce5a4bb918bd69fa3f95d9aa77
evlosh <evlosh@gmail.com>
2023-01-18 23:03:12 +0300

    доработай

--- test.py
+++ test.py
@@ -11,9 +11,10 @@ def get_question_word(words_list):
             if word in v:
                 question_type['key'] = k
                 parsed = morph.parse(word)[0].tag
+                question_type['pos'] = parsed.POS
                 question_type['number'] = parsed.number
-                question_type['gender'] = parsed.gender
                 question_type['case'] = parsed.case
+                question_type['gender'] = parsed.gender
                 return question_type
 
 
@@ -23,27 +24,35 @@ def get_prep(words_list):
         if parsed == 'PREP':
             return word
 
-# message = 'На каком члене ты скакала?'
-# words_list = words_regex(message.lower())
+message = 'Во сколько лет я умру?'
+words_list = words_regex(message.lower())
 
-# question_type = get_question_word(words_list)
-# prep = get_prep(words_list)
-
-# answ_src = random.choice(destiny_answer[question_type['key']])
-# answ_res = []
-# if prep:
-#     answ_res.append(prep)
-
-# for i in answ_src.split():
-
-#     parsed = morph.parse(i)[0]
-#     res = parsed.inflect({
-#         question_type['number'], question_type['gender'], question_type['case']})
-#     if res:
-        # answ_res.append(res.word)
+question_type = get_question_word(words_list)
+prep = get_prep(words_list)
 
 
+answ_src = random.choice(destiny_answer[question_type['key']])
+answ_res = []
+if prep:
+    prep = prep.replace('во', 'в')
+    answ_res.append(prep)
+
+for i in answ_src.split():
+    parsed = morph.parse(i)[0]
+
+    res = parsed.inflect({
+        question_type['number'], question_type['case']})
+
+    if question_type['gender']:
+        parsed = morph.parse(res.word)[0]
+        res = parsed.inflect({question_type['gender']})
+
+    if res:
+        answ_res.append(res.word)
+    else:
+        answ_res.append(i)
 
 print()
+print(message)
 print(' '.join(answ_res).capitalize())
 print()
