98005d58610454a1ada725d701cc6e63f66bd00c
Evgeny Loshkarev <evlosh@gmail.com>
2023-01-27 10:50:52 +0300

    merge queries in one

--- test.py
+++ test.py
@@ -1,12 +1,22 @@
-data = [('день', 116), ('год', 103), ('просто', 97), ('просто', 90), ('хороший', 86), ('весь', 80), ('40', 79), ('dsp', 77), ('вообще', 75), ('хороший', 72)]
+import sqlalchemy
+from test_db import *
 
 
+sq_sum = sqlalchemy.sql.expression.func.sum
+sq_count = sqlalchemy.sql.expression.func.count
+chat_id = 1
 
-from datetime import datetime
+queries = []
 
-dt = datetime.strptime(str(datetime.today()), '%Y-%m-%d %H:%M:%S')
+for user_id in (1, 2):
+    q = (
+        sqlalchemy.select(Words.user_id, sq_sum(Words.count), sq_count(Words.word))
+            .filter(Words.user_id==user_id, Words.chat_id==chat_id)
+            )
+    queries.append(q)
 
-print(dt)
+query = sqlalchemy.union(*queries)
 
+res = [dict(i) for i in Dbase.conn.execute(query).fetchall()]
 
-print(datetime.today())
+print(res)
