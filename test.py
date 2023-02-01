b776dd2961814c97d01e2e94cb598c33f4d731e8
Evgeny Loshkarev <evlosh@gmail.com>
2023-02-01 12:59:42 +0300

    testing queries

--- test.py
+++ test.py
@@ -5,3 +5,42 @@
 #     bot = ChatGPT()
 #     return bot.ask("Когда появился человек?")
 
+
+
+import sqlalchemy
+
+from test_db import Dbase, Users, Words
+
+
+def db_insert(new_words: dict):
+    vals = [
+        {
+        "b_user_id": user_id, "b_chat_id": chat_id,
+        "b_word": word, "b_count": count
+        }
+        for (user_id, chat_id), words in new_words.items()
+        for word, count in words.items()
+        ]
+
+    q = (
+        sqlalchemy.insert(Words)
+        .values({
+            'word': sqlalchemy.bindparam("b_word"),
+            'count': sqlalchemy.bindparam("b_count"),
+            'user_id': sqlalchemy.bindparam("b_user_id"),
+            'chat_id': sqlalchemy.bindparam("b_chat_id")
+            })
+            )
+
+    Dbase.conn.execute(q, vals)
+
+
+long_dict = {}
+for i in range(10000):
+    long_dict[(i, i)] = {"hui": 3}
+
+# print(len(long_dict))
+
+db_insert(long_dict)
+
+
