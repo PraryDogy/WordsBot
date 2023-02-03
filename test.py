dd1afcb60cac850092bde3027b7740bfb956bfbe
Evgeny Loshkarev <evlosh@gmail.com>
2023-02-03 14:43:12 +0300

    wrappers

--- test.py
+++ test.py
@@ -1,22 +1,41 @@
 import cfg
 from start_utils import user_data
+from functools import wraps
 
 
 
-def dec_user_data(func):
-    def wrapper(**kw):
-        kw.update(user_data(kw["user_id"], kw["user_name"]))
-        return func(**kw)
+def dec_words_data(func):
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print("i'm write words and return function to next actions")
+        return func(**kwargs)
+
     return wrapper
 
 
+def dec_user_data(func):
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        kwargs.update(
+            user_data(kwargs["user_id"], kwargs["user_name"])
+            )
+        return func(**kwargs)
+
+    return wrapper
+
+
+@dec_words_data
 @dec_user_data
-def handler_func(**kw):
-    print(kw["user_time"])
+def handler_func(*args, **kwargs):
+    print(kwargs)
 
 
-handler_func(
-    user_id = cfg.evlosh,
-    user_name = "pter",
-    limit = 500
-    )
+handler_func(user_id = cfg.evlosh, user_name = "evlosh", limit = 500)
+
+
+import tkinter
+
+
+tkinter.Button()
